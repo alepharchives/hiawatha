1c75d65a7b9c32987010a333a6e2a2b51f70909b
Hugo Leisink <hugo@leisink.net>
2013-02-19 09:44:02 +0100

    Release 8.8

--- build/polarssl/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hugo/projects/hiawatha/polarssl
-# Build directory: /home/hugo/projects/hiawatha/build/polarssl
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(library)
--- build/polarssl/library/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hugo/projects/hiawatha/polarssl/library
-# Build directory: /home/hugo/projects/hiawatha/build/polarssl/library
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
